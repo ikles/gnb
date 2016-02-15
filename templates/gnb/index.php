370ca43a59be1c4b425fd3f91d3df8d2b4d46085
Igor Kleshev <membship@ya.ru>
2016-02-15 22:41:58 +0600

    Commit

--- templates/gnb/index.php
+++ templates/gnb/index.php
@@ -13,19 +13,14 @@ $app = JFactory::getApplication();
 <script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery-1.7.2.min.js"></script>
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/form.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css" type="text/css" />
 </head>
-
 <body style="margin:0px" bgcolor="#FFFFFF">
-<div class="knop">
-<div class="knop1"><a href="/zadat-vopros"></a></div>
-<div class="knop2"><a href="/sdelat_zayavku"></a></div>
-</div>
-
+<jdoc:include type="modules" name="knop" style="mystyle" />
 <table width="100%" border="0" cellspacing="0" cellpadding="0" style="height:100%">
   <tr>
-    <td height="153" valign="top"><div style="position: relative; width: 100%; height:153px;"><table   class="header_tb" width="100%" border="0" cellpadding="0" cellspacing="0" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/pic_bg.jpg" style="background-repeat:repeat-x" bgcolor="#EDEDF0">
-    
+    <td height="153" valign="top"><div style="position: relative; width: 100%; height:153px;"><table   class="header_tb" width="100%" border="0" cellpadding="0" cellspacing="0" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/pic_bg.jpg" style="background-repeat:repeat-x" bgcolor="#EDEDF0">   
   <tr>
         <td><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/pic1.jpg" width="290" height="120"></td>
         <td width="100%"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/pic2.jpg" width="200" height="120"></td>
@@ -37,13 +32,11 @@ $app = JFactory::getApplication();
         <tr>
           <td width="240" height="33" valign="top"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/up_m1.jpg" width="240" height="33"></td>
           <td width="210" height="33" valign="top"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/up_m2.jpg" width="210" height="33"></td>
-          <td height="33" align="right" style="padding-right:70px"><table width="0%" border="0" cellspacing="0" cellpadding="0">
-            <tr>
-              <td class="menu2"><a class="mnu" href="/about">О компании</a></td><td class="menu2"><a class="mnu" href="/partnery">Партнеры</a></td><td class="menu2"><a class="mnu" href="/stati">Статьи</a></td><td class="menu2"><a class="mnu" href="/zadat-vopros">Задать вопрос</a></td><td class="menu2"><a class="mnu" href="/sdelat_zayavku">Оставить заявку</a></td><td class="menu2"><a class="mnu" href="/contacts">Наш адрес</a></td>            </tr>
-          </table></td>
+          <td height="33" align="right" style="padding-right:70px">
+          <jdoc:include type="modules" name="top_menu" style="mystyle" />
+          </td>
         </tr>
       </table>
-
 </td>
   </tr>
   <tr>
@@ -51,27 +44,16 @@ $app = JFactory::getApplication();
       <tr>
         <td width="240" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
           <tr>
-            <td height="41" align="center" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/search.jpg"><form action="/search" method="GET" name="" id="">
-<input name="search" type="text" style="border: 1px solid #717171;width:120px;height:18px;font-size:10px;" value="быстрый поиск ..." />&nbsp;
-<input type="submit" style="border: 1px solid #F00000;height:18px;font-size:10px; background-color:#F00000; cursor:pointer; color:#FFFFFF"  value="поиск" />
-</form></td>
+            <td height="41" align="center" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/search.jpg">
+            <jdoc:include type="modules" name="search" style="mystyle" />
+            </td>
           </tr>
           <tr>
-            <td height="72" valign="top" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/left_mup.jpg" style="background-repeat:no-repeat; background-position:top; padding:15px 11px 40px 30px"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                          
-                               <tr>
-                <td class="menu"><a class="mnu" href="/bestransheynye_tehnologi" >Бестраншейные технологии</a></td></tr>
-                                                        
-                               <tr>
-                <td class="menu"><a class="mnu" href="/remont_podzemnyh_kommunikaciy__bestransheynym_sposobom" >Ремонт подземных коммуникаций  бестраншейным способом</a></td></tr>
-                                                        
-                               <tr>
-                <td class="menu"><a class="mnu" href="price" >Прайс-лист</a></td></tr>
-                                          
-            </table></td>
-<div class="tel_top"><p>Тел. в Москве: <span style="font-weight: bold; font-size: 16px; background: rgba(255,255,255, 0.5);">8 (495) 543-15-00; 8 (905) 543-15-00<br /></span></p></div>
-<div class="form_top"><a href="/zakazat-obratnyy-zvonok?aj" onclick="return hs.htmlExpand( this, {objectType: 'iframe', outlineType: 'rounded-white', wrapperClassName: 'highslide-wrapper drag-header',
-        outlineWhileAnimating: true, preserveContent: false, width: 450 } )">заказать обратный звонок</a></div>
+            <td height="72" valign="top" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/left_mup.jpg" style="background-repeat:no-repeat; background-position:top; padding:15px 11px 40px 30px">
+            <jdoc:include type="modules" name="left_menu" style="mystyle" />
+          </td>
+<jdoc:include type="modules" name="tel_top" style="mystyle" />
+<jdoc:include type="modules" name="form_top" style="mystyle" />
 </div>
           </tr>
           <tr>
@@ -87,94 +69,46 @@ $app = JFactory::getApplication();
           <tr>
             <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr>
-                <td  class="name" align="center">Наши контакты</td>
+                <td  class="name" align="center"></td>
               </tr>
               <tr>
-                <td valign="top" class="tel" style="padding:10px 12px 40px 34px"><div style="text-align: center;">
-<p><strong>Адрес</strong>: <br />123060, г. Москва, <br />ул.&nbsp;Расплетина, 19, офис 2</p>
-<p><strong>Телефоны</strong>:<br /><span style="color: #888888;">8 (495)</span> <span style="font-size: 13pt;">543-15-00</span><span style="color: #888888;"><br /></span><span style="font-size: 13pt;"></span></p>
-</div></td>
+                <td valign="top" class="tel" style="padding-left: 24px">
+                <jdoc:include type="modules" name="left_contacts" style="mystyle" />
+</td>
               </tr>
             </table></td>
           </tr>
         </table></td>
-        <td valign="top" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/body_up.jpg" style="background-repeat:no-repeat; background-position:top left"><table width="100%" border="0" cellspacing="0" cellpadding="0" style="height:100%">
+        <td valign="top" background="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/body_up.jpg" style="background-repeat:no-repeat; background-position:top left">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0" style="height:100%">
           <tr>
-            <td height="39" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <td height="39" valign="top">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr>
-                <td height="24"><h1 class="tema">О компании</h1></td>
+                <td height="24"></td>
               </tr>
               <tr>
-                <td height="15" style="padding-left:27px"><table width="0%" border="0" cellspacing="0" cellpadding="0">
-              <tr>
-                <td class="ban"><a class="b" href="/">Главная</a></td>
-                <td><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/arr.gif" width="15" height="7"></td>
-                <td class="ban"> <a class="d" href="/about">О компании</a>
-</td>
-<td><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/arr.gif" width="15" height="7"></td>
-              </tr>
-            </table></td>
+                <td height="15">
+            
+              <jdoc:include type="modules" name="bread" style="mystyle" />
+
+            </td>
               </tr>
             </table></td>
           </tr>
           <tr>
-            <td valign="top" class="body">
-<p>Добро пожаловать на сайт компании &laquo;Бестраншейные технологии&raquo;!</p>
-<p>Современный подход к ремонту и прокладке наружных инженерных коммуникаций и трубопроводов требует применения инновационных технологий, не нарушающих стабильный ритм жизни населенных пунктов и не наносящих ущерба окружающей среде.</p>
-<p><img alt="для главной" border="1" class="round-shape" height="208" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/2254603601_7.jpg" style="display: block; margin: 20px auto;" width="580" /></p>
-<p>Наша компания специализируется на качественном строительстве, реконструкции и ремонте инженерных сетей различного назначения бестраншейным способом. Мы выполняем полный цикл работ:</p>
-<ul>
-<li>монтажные работы (без вскрытия верхнего слоя почвы);</li>
-<li>пуско-наладочные мероприятия;</li>
-<li>сдача нового или реконструированного объекта в эксплуатацию.</li>
-</ul>
-<p>&nbsp;</p>
-<p style="text-align: center;"><strong>Наши услуги, осуществляемые бестраншейным способом</strong></p>
-<p>&nbsp;</p>
-<div style="margin: 30px auto; width: 90%;">
-<div class="usluga"><a href="/remont_podzemnyh_kommunikaciy__bestransheynym_sposobom" class="novaja">Ремонт подземных коммуникаций</a></div>
-<div class="usluga"><a href="/remont_vodoprovoda" class="novaja">Ремонт водопровода</a></div>
-<div class="usluga"><a href="/remont_kanalizacii" class="novaja">Ремонт канализации</a></div>
-<div class="usluga"><a href="/bestransheynaya-prokladka-kommunikacij" class="novaja">Прокладка коммуникаций</a></div>
-<div class="usluga"><a href="/bestransheynaya-prokladka-kabelya" class="novaja">Прокладка кабеля</a></div>
-<div class="usluga"><a href="/bestransheynaya-prokladka-trub" class="novaja">Прокладка труб</a></div>
-</div>
-<div align="center">
-<div class="uslugasanacija"><a href="/bestransheynaya-sanaciya-truboprovodov" class="novaja">Санация трубопроводов</a></div>
-<div class="uslugasanacija"><a href="/bestransheynaya-zamena-truboprovodov" class="novaja">Замена трубопроводов</a></div>
-</div>
-<div style="clear: both;"></div>
-<p>Бестраншейный метод возведения и ремонта наружных инженерных коммуникаций, кабелей, трубопроводов имеет массу преимуществ перед традиционными технологиями:</p>
-<ul>
-<li>нет необходимости перекрывать движение транспорта и пешеходов в зоне ведения работ &mdash; город продолжает жить в привычном режиме;</li>
-<li>окружающая среда претерпевает минимальный ущерб, так как верхний слой почвы не вскрывается;</li>
-<li>не наносится никакого вреда существующим наземным коммуникациям;</li>
-<li>стоимость работ оценивается в 2,5-3 раза меньше, чем при прокладке траншей.</li>
-</ul>
-<h2>Преимущества сотрудничества с нашей компанией</h2>
-<p>Узкая специализация дает нам возможность сконцентрировать в коллективе компании высокопрофессиональных специалистов в области возведения и реконструкции инженерных коммуникаций. Благодаря огромному опыту работы, мы готовы оперативно и в полном объеме выполнять любые тематические поставленные задачи.</p>
-<div class="plus">
-<p>Компания &laquo;Бестраншейные технологии&raquo;:</p>
-<ul>
-<li>применяет в строительстве инженерных коммуникаций передовые методики;</li>
-<li>имеет собственную техническую базу, мощную и современную;</li>
-<li>использует в работе исключительно высококачественные материалы;</li>
-<li>предоставляет гарантию качества на все виды выполняемых работ.</li>
-</ul>
-</div>
-<p>Получить исчерпывающую информацию об особенностях технологии и вариантах обоюдовыгодного сотрудничества Вы в любой момент можете у наших консультантов-менеджеров.</p>
-<div style="margin: 30px auto; width: 100%;">
-<div class="kartinki"><a href="/contacts"><img alt="Наш номер телефона" height="124" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/phone_0.png" title="Наш номер телефона" width="134" /></a> <span class="kont">8 (495) 543-15-00</span></div>
-<div class="kartinki"><a href="/contacts"><img alt="Наш адрес электронной почты" height="124" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/mail_0.png" title="Наш адрес электронной почты" width="134" /></a> <span class="kont">sviazstroy@rambler.ru</span></div>
-<div class="kartinki"><a href="/price"><img alt="Наш прайс-лист" height="124" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/img/price__0.png" title="Наш прайс-лист" width="134" /></a> <a href="/price" title="Узнать цены на наши услуги" class="novajatext">Прайс-лист</a></div>
-<div style="clear: both;"></div>
-</div>
-<p>ООО &laquo;Бестраншейные технологии&raquo; &mdash; Ваш надежный и перспективный партнер!</p>
-
+<td valign="top" class="body">
+<div id="wrapper">
+<jdoc:include type="component" style="xhtml" />
+<jdoc:include type="modules" name="phone_mail_price" style="mystyle" />
+<jdoc:include type="modules" name="form_static" style="mystyle" />
+<jdoc:include type="modules" name="form_dinamic" style="mystyle" />
 <script type="text/javascript" src="//yandex.st/share/share.js"
     charset="utf-8"></script>
     <div style="clear:both; height: 1px;">&nbsp;</div>
-    <div style="text-align: right;" class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,lj,gplus" ></div> </td>
+    <div style="text-align: right;" class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,lj,gplus" ></div>
+  </div>
+  </td><!--class body-->
           </tr>
         </table></td>
       </tr>
@@ -186,15 +120,9 @@ $app = JFactory::getApplication();
         <td width="400" height="91" align="center" class="small"><!--noindex--><!-- begin of Top100 code --> 
 <a rel="nofollow" href="http://top100.rambler.ru/top100/"><img src="http://counter.rambler.ru/top100.cnt?1395177" alt="Rambler's Top100" width="1" height="1" border="0" /></a>
 <!-- end of Top100 code --> 
-
-
 <!-- begin of Top100 logo --> 
 <a rel="nofollow" href="http://top100.rambler.ru/top100/"><img src="http://top100-images.rambler.ru/top100/banner-88x31-rambler-gray2.gif" alt="Rambler's Top100" width="88" height="31" border="0" /></a> 
 <!-- end of Top100 logo -->
-
-
-
-
 <!--LiveInternet counter--><script type="text/javascript"><!--
 document.write("<a href='http://www.liveinternet.ru/click' "+
 "target=_blank><img src='http://counter.yadro.ru/hit?t14.1;r"+
@@ -205,21 +133,14 @@ screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
 "' alt='' title='LiveInternet: показано число просмотров за 24"+
 " часа, посетителей за 24 часа и за сегодня' "+
 "border=0 width=88 height=31><\/a>")//--></script><!--/LiveInternet-->
-
-
 <!--Rating@Mail.ru COUNTEr--><a rel="nofollow" target="_top" 
 href="http://top.mail.ru/jump?from=1424219"><img
 src="http://db.cb.b5.a1.top.list.ru/counter?id=1424219;t=196"
 border="0" height="31" width="38"
 alt="Рейтинг@Mail.ru"/></a><!--/COUNTER-->
-
 <a rel="nofollow" href=http://www.aport.ru/ target=_blank><img src=http://www.aport.ru//assets/a9ce2c4f/images/design/promo-new.jpg border=0 width=40 height=31></a>
-
 <br />
 <a rel="nofollow" href="http://www.yandex.ru/cy?base=0&amp;host=www.gnb-technology.ru"><img src="http://www.yandex.ru/cycounter?www.gnb-technology.ru" width="88" height="31" alt="Яндекс цитирования" border="0" /></a>
-
-
-
 <!--Hitmir counter--><script type="text/javascript"><!--
 js=1;
 document.write('<a href="http://hitmir.ru/click/317"'+
@@ -233,7 +154,6 @@ if(js=1)document.write('<'+'!-- ')//--></script><noscript><a href="http://hitmir
  border=0 width=88 height=31></a></noscript><script type="text/javascript"><!--
 if(js=1)document.write('--'+'>')//--></script>
 <!--/Hitmir--> 
-
 <!-- Yandex.Metrika counter -->
 <div style="display:none;"><script type="text/javascript">
 (function(w, c) {
