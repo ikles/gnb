74f170ec6e3ebe96115402ea581e994c082ab990
Igor Kleshev <membship@ya.ru>
2016-02-15 13:41:36 +0700

    1

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-02-15 06:34:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-02-15T06:34:44+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-02-15T06:34:53+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-02-15T06:36:42+00:00	INFO 127.0.0.1	joomlafailure	Имя пользователя и пароль не совпадают или у вас ещё нет учётной записи на сайте
